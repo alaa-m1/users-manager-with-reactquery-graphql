10ae64c22d97dcf4cfa220c9a645beb073031367
Alaa-M1 <alaa85a@gmail.com>
2023-05-15 15:21:59 +0200

    update Apollo Client to get benefit from Apollo object cache id

--- src/utils/apollo/ApolloGenericProvider.tsx
+++ src/utils/apollo/ApolloGenericProvider.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider, IdGetter } from "@apollo/client";
 import { mongoDBGraphQLUrl } from "utils/constants";
+import { KeySpecifier } from "@apollo/client/cache/inmemory/policies";
 
 const ApolloGenericProvider = ({ children }: { children: React.ReactNode }) => {
   const client = new ApolloClient({
     uri: mongoDBGraphQLUrl,
-    cache: new InMemoryCache(),
+    cache: new InMemoryCache({ dataIdFromObject(responseObject){ 
+      const res= responseObject.id as  KeySpecifier | false | ReturnType<IdGetter>
+      return res} }),
   });
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
 };
